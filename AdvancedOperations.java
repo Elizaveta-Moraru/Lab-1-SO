f21357b47513f210a2f550531d46283d123d845e
Elizaveta-Moraru <156651011+Elizaveta-Moraru@users.noreply.github.com>
2024-09-26 10:32:42 +0300

    Add files via upload

---
+++ AdvancedOperations.java
@@ -0,0 +1,20 @@
+public class AdvancedOperations {
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public double sqrt(double a) {
+        if (a < 0) {
+            throw new ArithmeticException("Square root of negative number is not allowed.");
+        }
+        return Math.sqrt(a);
+    }
+}
